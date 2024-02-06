946444ac3eea6d8daf3a07a6dbaa7c6dfc64a881
Althaaff <muhammadalthaf228@gmail.com>
2024-02-06 18:06:10 +0530

    Stop Watch

---
+++ Stop Watch/app.js
@@ -0,0 +1,54 @@
+let [seconds, minutes, hours] = [0,0,0];
+let displayTime = document.getElementById('displayTime');
+let timer = null;
+let pauseBtn = document.querySelector('#pause');
+let isPlay = true;
+
+
+function stopWatch() {
+    seconds++;
+    if (seconds == 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes == 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+
+    displayTime.innerHTML = h +":"+ m +":"+s;
+}
+
+function watchStart() {
+    if (timer !== null) {
+        clearInterval(timer);
+    }
+   
+   timer = setInterval(stopWatch,1000);
+}
+
+function watchStop() {
+    clearInterval(timer);
+}
+
+function watchReset() {
+    clearInterval(timer);
+    [seconds, minutes, hours] = [0, 0, 0];
+    displayTime.innerHTML = "00:00:00";
+}
+
+pauseBtn.addEventListener('click', () => {
+    if (isPlay) {
+        watchStart(); // Start the timer
+        pauseBtn.src = "images/pause.png"; // Change the image to pause
+    } else {
+        watchStop(); // Stop the timer
+        pauseBtn.src = "images/play.png"; // Change the image to play
+    }
+    isPlay = !isPlay; // Toggle the state
+});
+
